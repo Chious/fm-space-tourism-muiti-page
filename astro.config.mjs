6c35f5ac9128ffe92dbfa314e05b88b7c71cd721
chious <chu23294752@gmail.com>
2025-10-27 13:19:51 +0800

    style: Add Tailwind CSS and project assets
    - Integrated Tailwind CSS with Astro by `npx add tailwind`
    
    - updating astro.config.mjs for Vite plugin and path aliases
    
    - desing assets import: added image and SVG assets for crew, destination, home, and technology sections, as well as favicon updates.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+      }
+    }
+  }
+});
